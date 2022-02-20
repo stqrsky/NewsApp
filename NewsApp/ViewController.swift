2175872ffcc44473fe2d39c119ce32fd026b1c89
Starsky <starsky.m@live.com>
2022-02-20 13:52:43 +0100

    prgrammatic UI

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hey guys"
+        label.tintColor = .systemRed
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+        configureUI()
+        
+    }
+    
+    private func configureUI() {
+        label.text = "Enjoy everybody!"
+//        label.textAlignment = .center
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//            label.widthAnchor.constraint(equalToConstant: 200),
+//            label.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 
 
