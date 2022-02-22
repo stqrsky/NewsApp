8a9fe141fc1b41c472ee141d22bbb94198125b41
Starsky <starsky.m@live.com>
2022-02-22 13:45:35 +0100

    titleLabel and subTitleLabel

--- NewsApp/Cells/NewsTableViewCell.swift
+++ NewsApp/Cells/NewsTableViewCell.swift
@@ -10,6 +10,22 @@ import UIKit
 class NewsTableViewCell: UITableViewCell {
     
     static let reuseID = "newsCell"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    
+    private let titleStackView = UIStackView()
 
     override func awakeFromNib() {
         super.awakeFromNib()
